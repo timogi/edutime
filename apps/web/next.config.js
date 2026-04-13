17a467e1e937237b0e83a682ab0d92136afb0723
Tim Ogi <ogi.tim@bluewin.ch>
2026-04-13 15:32:11 +0200

    fix sentry

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -64,11 +64,6 @@ module.exports = withSentryConfig(module.exports, {
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
@@ -78,14 +73,20 @@ module.exports = withSentryConfig(module.exports, {
   // Hides source maps from generated client bundles
   hideSourceMaps: true,
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
+  // Webpack-only Sentry build options (see @sentry/nextjs SentryBuildOptions.webpack)
+  webpack: {
+    // Automatically tree-shake Sentry SDK logger statements (replaces deprecated disableLogger)
+    treeshake: {
+      removeDebugLogging: true,
+    },
+    // Automatically annotate React components for breadcrumbs / session replay (replaces top-level option)
+    reactComponentAnnotation: {
+      enabled: true,
+    },
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // https://docs.sentry.io/product/crons/ · https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+  },
 
   // Source map configuration
   sourcemaps: {
